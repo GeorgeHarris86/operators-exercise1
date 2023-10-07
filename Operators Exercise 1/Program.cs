a01e8e0f7d8c571042545a1418b7ffa3ed8a41d3
GeorgeHarris86 <geoharr21@gmail.com>
2023-10-07 08:22:04 -0400

    finised operators exercise 1

---
+++ Operators Exercise 1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace OperatorsExercise1
+{
+
+
+
+    class Program
+    {
+
+
+
+
+        static void Main(string[] args)
+        {
+            int f = 7;
+            int d = 8;
+
+            Console.WriteLine(f + d);
+
+
+            int x = 9;
+            int g = 3;
+
+            Console.WriteLine(x - g);
+
+            int z = 5;
+            int t = 2;
+
+            Console.WriteLine(z * t);
+
+            int a = 16;
+            int b = 5;
+
+            Console.WriteLine(a / b);
+
+            Console.WriteLine("if a = 16 and b= 5 (16/5) = 3 remainder is 2");
+
+        }
+
+    }
+
+    
+    
+
+
+
+
+
+
+}
